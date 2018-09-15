282d1d387161ad8f03423f1b30a992e9b1c21176
Kusvihawan <39431395+Kusvihawan@users.noreply.github.com>
2018-09-15 07:56:34 +0700

    ini baru joe

---
+++ Project/Tower Deffense/Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointsIndex = 0;
+
+    private void Start()
+    {
+        target = Tujuans.points[0];
+
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate();
+    }
+}
